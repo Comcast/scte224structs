2c7f81393b4e277e8db7e27a8f98729d2aeccceb
Jonathan Colwell <jonathan.colwell@comcast.com>
2018-07-19 15:44:13 -0700

    improving the roundtrip test to actually test the roundtrip

--- types/scte224v20151115/roundtrip_test.go
+++ types/scte224v20151115/roundtrip_test.go
@@ -6,29 +6,30 @@ import (
 	"testing"
 )
 
-const CALI_XML = `<Media xmlns:audience="urn:scte:224:audience" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:action="urn:scte:224:action" xmlns="http://www.scte.org/schemas/224/2015" id="superflaco.com/media/CALIFORNIA/BROADCAST" description="CALIFORNIA" lastUpdated="2018-05-29T00:44:57.000Z" source="CURRENT_CHANNEL">
-<MediaPoint id="superflaco.com/media/CALIFORNIA/program/AIRING_ID/BROADCAST/start" description="I am a MediaPoint" lastUpdated="2018-05-08T00:49:37.000Z" effective="2018-05-29T00:00:00.000Z" expires="2018-05-29T10:00:00.000Z" source="CURRENT_CHANNEL">
-<AltID>CID:e0be5016-5ac2-4c36-8cef-fc00ce40372e</AltID>
-<Metadata>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Sport" provider="superflaco" type="string">Documentary</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Lookback" provider="superflaco" type="bool">false</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Prg_dai_enabled" provider="superflaco" type="bool">false</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Startover" provider="superflaco" type="bool">false</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="ScheduledStart" provider="superflaco" type="string">2018-05-29T01:00:00Z</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="ScheduledAiringID" provider="superflaco" type="string">AIRING_ID</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Embargo" provider="superflaco" type="string">false</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Prg_dai_devices" provider="superflaco" type="bool">false</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="SeriesEmbargo" provider="superflaco" type="string">false</MetadataDetail>
-<MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="ScheduledEnd" provider="superflaco" type="string">2018-05-29T01:30:00Z</MetadataDetail>
-</Metadata>
-<Apply>
-<Policy xlink:href="superflaco.com/policy/SWITCH_to_CA"/>
-</Apply>
-<MatchSignal match="ANY">
-<Assert>/SpliceInfoSection/SegmentationDescriptor[@segmentationTypeId=16]/SegmentationUpid[@segmentationUpidType=1 and text()='AIRING_ID']</Assert>
-<Assert>/SpliceInfoSection/SegmentationDescriptor[@segmentationTypeId=1]/SegmentationUpid[@segmentationUpidType=1 and text()='AIRING_ID']</Assert>
-</MatchSignal>
-</MediaPoint></Media>`
+const CALI_XML = `<Media xmlns="http://www.scte.org/schemas/224/2015" id="superflaco.com/media/CALIFORNIA/BROADCAST" description="CALIFORNIA" lastUpdated="2018-05-29T00:44:57Z" source="CURRENT_CHANNEL">
+  <MediaPoint xmlns="http://www.scte.org/schemas/224/2015" id="superflaco.com/media/CALIFORNIA/program/AIRING_ID/BROADCAST/start" description="I am a MediaPoint" lastUpdated="2018-05-08T00:49:37Z" effective="2018-05-29T00:00:00Z" expires="2018-05-29T10:00:00Z" source="CURRENT_CHANNEL">
+    <AltID xmlns="http://www.scte.org/schemas/224/2015">CID:e0be5016-5ac2-4c36-8cef-fc00ce40372e</AltID>
+    <Metadata xmlns="http://www.scte.org/schemas/224/2015">
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Sport" type="string" provider="superflaco">Documentary</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Lookback" type="bool" provider="superflaco">false</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Prg_dai_enabled" type="bool" provider="superflaco">false</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Startover" type="bool" provider="superflaco">false</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="ScheduledStart" type="string" provider="superflaco">2018-05-29T01:00:00Z</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="ScheduledAiringID" type="string" provider="superflaco">AIRING_ID</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Embargo" type="string" provider="superflaco">false</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="Prg_dai_devices" type="bool" provider="superflaco">false</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="SeriesEmbargo" type="string" provider="superflaco">false</MetadataDetail>
+      <MetadataDetail xmlns="http://ctsrmm.com/ctsesni" name="ScheduledEnd" type="string" provider="superflaco">2018-05-29T01:30:00Z</MetadataDetail>
+    </Metadata>
+    <Apply xmlns="http://www.scte.org/schemas/224/2015">
+      <Policy xmlns="http://www.scte.org/schemas/224/2015" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="superflaco.com/policy/SWITCH_to_CA"></Policy>
+    </Apply>
+    <MatchSignal xmlns="http://www.scte.org/schemas/224/2015" match="ANY">
+      <Assert xmlns="http://www.scte.org/schemas/224/2015">/SpliceInfoSection/SegmentationDescriptor[@segmentationTypeId=16]/SegmentationUpid[@segmentationUpidType=1 and text()=&#39;AIRING_ID&#39;]</Assert>
+      <Assert xmlns="http://www.scte.org/schemas/224/2015">/SpliceInfoSection/SegmentationDescriptor[@segmentationTypeId=1]/SegmentationUpid[@segmentationUpidType=1 and text()=&#39;AIRING_ID&#39;]</Assert>
+    </MatchSignal>
+  </MediaPoint>
+</Media>`
 
 func TestRoundtrip(t *testing.T) {
 
@@ -53,5 +54,11 @@ func TestRoundtrip(t *testing.T) {
 		t.Log(marshalErr)
 		t.FailNow()
 	}
-	t.Log(string(pretty))
+	roundtrip := string(pretty)
+	if CALI_XML != roundtrip {
+		t.Log(roundtrip)
+		t.Log("did not match")
+		t.Log(CALI_XML)
+		t.Fail()
+	}
 }
