0e03b7aba93582a8b606690c1ce9013484038ed3
Jonathan Colwell <jonathan.colwell@comcast.com>
2018-07-27 14:44:36 -0700

    stripping out all structures not explicitly in the schema and leaving the metadata and extensions as unparsed XML

--- types/scte224v20151115/sctestructures.go
+++ types/scte224v20151115/sctestructures.go
@@ -20,7 +20,7 @@ type IdentifiableType struct {
 	XMLBase     string     `xml:"xml:base,attr,omitempty"`
 	AltIDs      []*AltID   `xml:"http://www.scte.org/schemas/224/2015 AltID,omitempty"`
 	Metadata    *Metadata  `xml:"http://www.scte.org/schemas/224/2015 Metadata,omitempty"`
-	Ext         string     `xml:"##other Ext,omitempty"`
+	Ext         *Metadata  `xml:"http://www.scte.org/schemas/224/2015 Ext,omitempty"`
 }
 
 //Table 5
@@ -44,17 +44,23 @@ type Media struct {
 //Table 7
 type MediaPoint struct {
 	IdentifiableType
-	XMLName     xml.Name     `xml:"http://www.scte.org/schemas/224/2015 MediaPoint"`
-	Effective   *time.Time   `xml:"effective,attr,omitempty"`
-	Expires     *time.Time   `xml:"expires,attr,omitempty"`
-	MatchTime   *time.Time   `xml:"matchTime,attr,omitempty"`
-	MatchOffset Duration     `xml:"matchOffset,attr,omitempty"`
-	Source      string       `xml:"source,attr,omitempty"`
-	Removes     []*Remove    `xml:"http://www.scte.org/schemas/224/2015 Remove"`
-	Applys      []*Apply     `xml:"http://www.scte.org/schemas/224/2015 Apply"`
-	MatchSignal *MatchSignal `xml:"http://www.scte.org/schemas/224/2015 MatchSignal"`
-	Order       uint         `xml:"-"` // used internally for ordering but not in the 2015 XSD
-	MediaGuid   string       `xml:"-"` // used internally to track which media this point is part of
+	XMLName          xml.Name     `xml:"http://www.scte.org/schemas/224/2015 MediaPoint"`
+	Effective        *time.Time   `xml:"effective,attr,omitempty"`
+	Expires          *time.Time   `xml:"expires,attr,omitempty"`
+	MatchTime        *time.Time   `xml:"matchTime,attr,omitempty"`
+	MatchOffset      Duration     `xml:"matchOffset,attr,omitempty"`
+	Source           string       `xml:"source,attr,omitempty"`
+	ExpectedDuration Duration     `xml:"-"` // not in the 2015 XSD
+	Order            uint         `xml:"-"` // used internally for ordering but not in the 2015 XSD
+	Reusable         bool         `xml:"-"` // not in the 2015 XSD
+	Removes          []*Remove    `xml:"http://www.scte.org/schemas/224/2015 Remove"`
+	Applys           []*Apply     `xml:"http://www.scte.org/schemas/224/2015 Apply"`
+	MatchSignal      *MatchSignal `xml:"http://www.scte.org/schemas/224/2015 MatchSignal"`
+	MediaGuid        string       `xml:"-"` // used internally to track which media this point is part of
+}
+
+type Metadata struct {
+	InnerXml string `xml:",innerxml"`
 }
 
 type Duration string
@@ -101,119 +107,16 @@ func ToDuration(dur time.Duration) Duration {
 }
 
 type AltID struct {
-	XMLName xml.Name `xml:"http://www.scte.org/schemas/224/2015 AltID"`
-	Value   string   `xml:",chardata"`
-}
-
-type Metadata struct {
-	XMLName        xml.Name          `xml:"http://www.scte.org/schemas/224/2015 Metadata"`
-	MetadataDetail []*MetadataDetail `xml:"http://ctsrmm.com/ctsesni MetadataDetail,omitempty"`
-	ScheduledStart *time.Time        `xml:"http://thistech.com/esni ScheduledStart,omitempty"`
-	ScheduledEnd   *time.Time        `xml:"http://thistech.com/esni ScheduledEnd,omitempty"`
-
-	//TODO: get this customer specific stuff out of here
-	//fox stuff
-	FoxMediaMetadata      *FoxMediaMetadata      `xml:"http://schema.foxinc.com/esni MediaMetaData,omitempty"`
-	ChannelMetadata       *ChannelMetadata       `xml:"http://schema.foxinc.com/esni ChannelMetadata,omitempty"`
-	DeviceMetadata        *DeviceMetadata        `xml:"http://schema.foxinc.com/esni DeviceMetadata,omitempty"`
-	LocationMetadata      *LocationMetadata      `xml:"http://schema.foxinc.com/esni LocationMetadata,omitempty"`
-	FoxMediaPointMetadata *FoxMediaPointMetadata `xml:"http://schema.foxinc.com/esni MediaPointMetaData,omitempty"`
-
-	//superflaco stuff
-	HomeTeam   string `xml:"http://xml.superflaco.com/listing/field homeTeam,omitempty"`
-	HomeTeamId string `xml:"http://xml.superflaco.com/listing/field homeTeamId,omitempty"`
-	AwayTeam   string `xml:"http://xml.superflaco.com/listing/field awayTeam,omitempty"`
-	AwayTeamId string `xml:"http://xml.superflaco.com/listing/field awayTeamId,omitempty"`
-	Sport      string `xml:"http://xml.superflaco.com/listing/field sport,omitempty"`
-
-	//A&E stuff
-	AScheduledStart    *time.Time `xml:"http://www.scte.org/schemas/224/2015 ScheduledStart,omitempty"`
-	AScheduledEnd      *time.Time `xml:"http://www.scte.org/schemas/224/2015 ScheduledEnd,omitempty"`
-	ProgramTitle       string     `xml:"http://www.scte.org/schemas/224/2015 ProgramTitle,omitempty"`
-	ProgramDescription string     `xml:"http://www.scte.org/schemas/224/2015 ProgramDescription,omitempty"`
-	EpisodeTitle       string     `xml:"http://www.scte.org/schemas/224/2015 EpisodeTitle,omitempty"`
-	EpisodeDescription string     `xml:"http://www.scte.org/schemas/224/2015 EpisodeDescription,omitempty"`
-}
-
-//Normalized stuff
-type MetadataDetail struct {
-	Name           string            `xml:"name,attr,omitempty"`
-	Type           string            `xml:"type,attr,omitempty"`
-	Provider       string            `xml:"provider,attr,omitempty"`
-	Value          string            `xml:",chardata"`
-	MetadataDetail []*MetadataDetail `xml:"http://ctsrmm.com/ctsesni MetadataDetail,omitempty"`
-}
-
-//FOX stuff
-type FoxMediaPointMetadata struct {
-	ScheduledAiringId    string                `xml:"http://schema.foxinc.com/esni ScheduledAiringId,omitempty"`
-	ScheduledStart       *time.Time            `xml:"http://schema.foxinc.com/esni ScheduledStart,omitempty"`
-	ScheduledEnd         *time.Time            `xml:"http://schema.foxinc.com/esni ScheduledEnd,omitempty"`
-	Category             string                `xml:"http://schema.foxinc.com/esni Category,omitempty"`
-	ProgramId            string                `xml:"http://schema.foxinc.com/esni ProgramId,omitempty"`
-	ProgramTitle         string                `xml:"http://schema.foxinc.com/esni ProgramTitle,omitempty"`
-	ProgramDescription   string                `xml:"http://schema.foxinc.com/esni ProgramDescription,omitempty"`
-	SeasonId             string                `xml:"http://schema.foxinc.com/esni SeasonId,omitempty"`
-	SeasonTitle          string                `xml:"http://schema.foxinc.com/esni SeasonTitle,omitempty"`
-	EpisodeId            string                `xml:"http://schema.foxinc.com/esni EpisodeId,omitempty"`
-	EpisodeTitle         string                `xml:"http://schema.foxinc.com/esni EpisodeTitle,omitempty"`
-	EpisodeDescription   string                `xml:"http://schema.foxinc.com/esni EpisodeDescription,omitempty"`
-	MaterialId           string                `xml:"http://schema.foxinc.com/esni MaterialId,omitempty"`
-	Rating               string                `xml:"http://schema.foxinc.com/esni Rating,omitempty"`
-	Live                 string                `xml:"http://schema.foxinc.com/esni Live,omitempty"`
-	StartOver            string                `xml:"http://schema.foxinc.com/esni StartOver,omitempty"`
-	LookBack             string                `xml:"http://schema.foxinc.com/esni LookBack,omitempty"`
-	DeliveryRestrictions *DeliveryRestrictions `xml:"http://schema.foxinc.com/esni DeliveryRestrictions,omitempty"`
-}
-
-type FoxMediaMetadata struct {
-	XMLName          xml.Name       `xml:"http://schema.foxinc.com/esni MediaMetaData"`
-	Network          *FoxAttributes `xml:"http://schema.foxinc.com/esni Network,omitempty"`
-	Region           *FoxAttributes `xml:"http://schema.foxinc.com/esni Region,omitempty"`
-	DistributionArea *FoxAttributes `xml:"http://schema.foxinc.com/esni DistributionArea"`
-}
-
-type ChannelMetadata struct {
-	XMLName          xml.Name       `xml:"http://schema.foxinc.com/esni ChannelMetadata"`
-	AudienceType     string         `xml:"http://schema.foxinc.com/esni AudienceType,omitempty"`
-	LocationType     string         `xml:"http://schema.foxinc.com/esni LocationType,omitempty"`
-	Network          *FoxAttributes `xml:"http://schema.foxinc.com/esni Network,omitempty"`
-	Region           *FoxAttributes `xml:"http://schema.foxinc.com/esni Region,omitempty"`
-	DistributionArea *FoxAttributes `xml:"http://schema.foxinc.com/esni DistributionArea,omitempty"`
-}
-
-type DeviceMetadata struct {
-	XMLName      xml.Name `xml:"http://schema.foxinc.com/esni DeviceMetadata"`
-	AudienceType string   `xml:"http://schema.foxinc.com/esni AudienceType,omitempty"`
-}
-
-type LocationMetadata struct {
-	XMLName      xml.Name `xml:"http://schema.foxinc.com/esni LocationMetadata"`
-	AudienceType string   `xml:"http://schema.foxinc.com/esni AudienceType,omitempty"`
-}
-
-type FoxAttributes struct {
-	Name         string `xml:"name,attr,omitempty"`
-	Call_sign    string `xml:"call_sign,attr,omitempty"`
-	Network_type string `xml:"network_type,attr,omitempty"`
-}
-
-type DeliveryRestrictions struct {
-	XMLName            xml.Name            `xml:"http://schema.foxinc.com/esni DeliveryRestrictions,omitempty"`
-	WebEnabled         bool                `xml:"http://schema.foxinc.com/esni WebEnabled,omitempty"`
-	DeviceRestrictions *DeviceRestrictions `xml:"http://schema.foxinc.com/esni DeviceRestrictions,omitempty"`
-}
-
-type DeviceRestrictions struct {
-	XMLName xml.Name `xml:"http://schema.foxinc.com/esni DeviceRestrictions,omitempty"`
-	Match   string   `xml:"http://schema.foxinc.com/esni match,attr,omitempty"`
-	Devices []*Item  `xml:"http://schema.foxinc.com/esni Device,omitempty"`
+	XMLName     xml.Name `xml:"http://www.scte.org/schemas/224/2015 AltID"`
+	Description string   `xml:"-"` // not in 2015 XSD
+	Value       string   `xml:",chardata"`
 }
 
 //Table 10
 type Apply struct {
 	XMLName  xml.Name `xml:"http://www.scte.org/schemas/224/2015 Apply"`
 	Duration Duration `xml:"duration,attr,omitempty"`
+	Priority uint     `xml:"-"` // not in 2015 XSD
 	Policy   *Policy  `xml:"http://www.scte.org/schemas/224/2015 Policy,omitempty"`
 }
 
@@ -253,65 +156,21 @@ type Policy struct {
 	ReusableType
 	XMLName        xml.Name         `xml:"http://www.scte.org/schemas/224/2015 Policy"`
 	ViewingPolicys []*ViewingPolicy `xml:"http://www.scte.org/schemas/224/2015 ViewingPolicy,omitempty"`
-	Metadata       string           `xml:"http://www.scte.org/schemas/224/2015 Metadata,omitempty"`
 }
 
 //Table 12
 type ViewingPolicy struct {
 	ReusableType
-	XMLName  xml.Name    `xml:"http://www.scte.org/schemas/224/2015 ViewingPolicy"`
-	Audience *Audience   `xml:"http://www.scte.org/schemas/224/2015 Audience,omitempty"`
-	Anys     []*Any      `xml:"http://www.scte.org/schemas/224/2015 Any,omitempty"`
-	Contents []*AContent `xml:"urn:scte:224:action Content,omitempty"`
-	FF       []*Item     `xml:"urn:scte:224:action FastForward,omitempty"`
-	Capture  []*Capture  `xml:"urn:scte:224:action Capture,omitempty"`
-	Metadata string      `xml:"http://www.scte.org/schemas/224/2015 Metadata,omitempty"`
+	XMLName  xml.Name  `xml:"http://www.scte.org/schemas/224/2015 ViewingPolicy"`
+	Audience *Audience `xml:"http://www.scte.org/schemas/224/2015 Audience,omitempty"`
 }
 
 //Table 13
 type Audience struct {
 	ReusableType
-	XMLName        xml.Name    `xml:"http://www.scte.org/schemas/224/2015 Audience"`
-	Match          Match       `xml:"match,attr,omitempty"`
-	Metadata       *Metadata   `xml:"http://www.scte.org/schemas/224/2015 Metadata,omitempty"`
-	Audiences      []*Audience `xml:"http://www.scte.org/schemas/224/2015 Audience,omitempty"`
-	Virds          []*Item     `xml:"urn:scte:224:audience Vird,omitempty"`
-	Zips           []*Item     `xml:"urn:scte:224:audience Zip,omitempty"`
-	Features       []*Item     `xml:"urn:scte:224:audience Feature,omitempty"`
-	DeviceFeatures []*Item     `xml:"urn:scte:224:audience DeviceFeature,omitempty"`
-	Devices        []*Item     `xml:"urn:scte:224:audience Device,omitempty"`
-	FoxDevices     []*Item     `xml:"http://schema.foxinc.com/esni Device,omitempty"`
-	Networks       []*Item     `xml:"urn:scte:224:audience Network,omitempty"`
-	Default        []*Item     `xml:"urn:scte:224:audience Default,omitempty"`
-}
-
-type Capture struct {
-	XMLName     xml.Name     `xml:"urn:scte:224:action Capture"`
-	StartWindow *StartWindow `xml:"urn:scte:224:action StartWindow,omitempty"`
-	StopWindow  *StopWindow  `xml:"urn:scte:224:action StopWindow,omitempty"`
-	MidrollDAI  *Item        `xml:"urn:scte:224:action MidrollDAI,omitempty"`
-}
-
-type StartWindow struct {
-	Percentage *Item `xml:"urn:scte:224:action Percentage,omitempty"`
-}
-
-type StopWindow struct {
-	Offset     *Item `xml:"urn:scte:224:action Offset,omitempty"`
-	Percentage *Item `xml:"urn:scte:224:action Percentage,omitempty"`
-}
-
-type AContent struct {
-	XMLName xml.Name `xml:"urn:scte:224:action Content"`
-	Value   string   `xml:",chardata"`
-}
-type Item struct {
-	Value string `xml:",chardata"`
-}
-
-type Any struct {
-	XMLName xml.Name `xml:"http://www.scte.org/schemas/224/2015 Any"`
-	Value   string   `xml:",chardata"`
+	XMLName   xml.Name    `xml:"http://www.scte.org/schemas/224/2015 Audience"`
+	Match     Match       `xml:"match,attr,omitempty"`
+	Audiences []*Audience `xml:"http://www.scte.org/schemas/224/2015 Audience,omitempty"`
 }
 
 //********************* Results Types *************************//
--- types/scte224v20180501/sctestructures.go
+++ types/scte224v20180501/sctestructures.go
@@ -20,7 +20,7 @@ type IdentifiableType struct {
 	XMLBase     string     `xml:"xml:base,attr,omitempty"`
 	AltIDs      []*AltID   `xml:"http://www.scte.org/schemas/224 AltID,omitempty"`
 	Metadata    *Metadata  `xml:"http://www.scte.org/schemas/224 Metadata,omitempty"`
-	Ext         string     `xml:"##other Ext,omitempty"`
+	Ext         *Metadata  `xml:"http://www.scte.org/schemas/224 Ext,omitempty"`
 }
 
 //Table 5
@@ -57,6 +57,10 @@ type MediaPoint struct {
 	Applys           []*Apply     `xml:"http://www.scte.org/schemas/224 Apply"`
 	MatchSignal      *MatchSignal `xml:"http://www.scte.org/schemas/224 MatchSignal"`
 	MediaGuid        string       `xml:"-"` // used internally to track which media this point is part of
+}
+
+type Metadata struct {
+	InnerXml string `xml:",innerxml"`
 }
 
 type Duration string
@@ -108,111 +112,6 @@ type AltID struct {
 	Value       string   `xml:",chardata"`
 }
 
-type Metadata struct {
-	XMLName        xml.Name          `xml:"http://www.scte.org/schemas/224 Metadata"`
-	MetadataDetail []*MetadataDetail `xml:"http://ctsrmm.com/ctsesni MetadataDetail,omitempty"`
-	ScheduledStart *time.Time        `xml:"http://thistech.com/esni ScheduledStart,omitempty"`
-	ScheduledEnd   *time.Time        `xml:"http://thistech.com/esni ScheduledEnd,omitempty"`
-
-	//TODO: get this customer specific stuff out of here
-	//fox stuff
-	FoxMediaMetadata      *FoxMediaMetadata      `xml:"http://schema.foxinc.com/esni MediaMetaData,omitempty"`
-	ChannelMetadata       *ChannelMetadata       `xml:"http://schema.foxinc.com/esni ChannelMetadata,omitempty"`
-	DeviceMetadata        *DeviceMetadata        `xml:"http://schema.foxinc.com/esni DeviceMetadata,omitempty"`
-	LocationMetadata      *LocationMetadata      `xml:"http://schema.foxinc.com/esni LocationMetadata,omitempty"`
-	FoxMediaPointMetadata *FoxMediaPointMetadata `xml:"http://schema.foxinc.com/esni MediaPointMetaData,omitempty"`
-
-	//superflaco stuff
-	HomeTeam   string `xml:"http://xml.superflaco.com/listing/field homeTeam,omitempty"`
-	HomeTeamId string `xml:"http://xml.superflaco.com/listing/field homeTeamId,omitempty"`
-	AwayTeam   string `xml:"http://xml.superflaco.com/listing/field awayTeam,omitempty"`
-	AwayTeamId string `xml:"http://xml.superflaco.com/listing/field awayTeamId,omitempty"`
-	Sport      string `xml:"http://xml.superflaco.com/listing/field sport,omitempty"`
-
-	//A&E stuff
-	AScheduledStart    *time.Time `xml:"http://www.scte.org/schemas/224 ScheduledStart,omitempty"`
-	AScheduledEnd      *time.Time `xml:"http://www.scte.org/schemas/224 ScheduledEnd,omitempty"`
-	ProgramTitle       string     `xml:"http://www.scte.org/schemas/224 ProgramTitle,omitempty"`
-	ProgramDescription string     `xml:"http://www.scte.org/schemas/224 ProgramDescription,omitempty"`
-	EpisodeTitle       string     `xml:"http://www.scte.org/schemas/224 EpisodeTitle,omitempty"`
-	EpisodeDescription string     `xml:"http://www.scte.org/schemas/224 EpisodeDescription,omitempty"`
-}
-
-//Normalized stuff
-type MetadataDetail struct {
-	Name           string            `xml:"name,attr,omitempty"`
-	Type           string            `xml:"type,attr,omitempty"`
-	Provider       string            `xml:"provider,attr,omitempty"`
-	Value          string            `xml:",chardata"`
-	MetadataDetail []*MetadataDetail `xml:"http://ctsrmm.com/ctsesni MetadataDetail,omitempty"`
-}
-
-//FOX stuff
-type FoxMediaPointMetadata struct {
-	ScheduledAiringId    string                `xml:"http://schema.foxinc.com/esni ScheduledAiringId,omitempty"`
-	ScheduledStart       *time.Time            `xml:"http://schema.foxinc.com/esni ScheduledStart,omitempty"`
-	ScheduledEnd         *time.Time            `xml:"http://schema.foxinc.com/esni ScheduledEnd,omitempty"`
-	Category             string                `xml:"http://schema.foxinc.com/esni Category,omitempty"`
-	ProgramId            string                `xml:"http://schema.foxinc.com/esni ProgramId,omitempty"`
-	ProgramTitle         string                `xml:"http://schema.foxinc.com/esni ProgramTitle,omitempty"`
-	ProgramDescription   string                `xml:"http://schema.foxinc.com/esni ProgramDescription,omitempty"`
-	SeasonId             string                `xml:"http://schema.foxinc.com/esni SeasonId,omitempty"`
-	SeasonTitle          string                `xml:"http://schema.foxinc.com/esni SeasonTitle,omitempty"`
-	EpisodeId            string                `xml:"http://schema.foxinc.com/esni EpisodeId,omitempty"`
-	EpisodeTitle         string                `xml:"http://schema.foxinc.com/esni EpisodeTitle,omitempty"`
-	EpisodeDescription   string                `xml:"http://schema.foxinc.com/esni EpisodeDescription,omitempty"`
-	MaterialId           string                `xml:"http://schema.foxinc.com/esni MaterialId,omitempty"`
-	Rating               string                `xml:"http://schema.foxinc.com/esni Rating,omitempty"`
-	Live                 string                `xml:"http://schema.foxinc.com/esni Live,omitempty"`
-	StartOver            string                `xml:"http://schema.foxinc.com/esni StartOver,omitempty"`
-	LookBack             string                `xml:"http://schema.foxinc.com/esni LookBack,omitempty"`
-	DeliveryRestrictions *DeliveryRestrictions `xml:"http://schema.foxinc.com/esni DeliveryRestrictions,omitempty"`
-}
-
-type FoxMediaMetadata struct {
-	XMLName          xml.Name       `xml:"http://schema.foxinc.com/esni MediaMetaData"`
-	Network          *FoxAttributes `xml:"http://schema.foxinc.com/esni Network,omitempty"`
-	Region           *FoxAttributes `xml:"http://schema.foxinc.com/esni Region,omitempty"`
-	DistributionArea *FoxAttributes `xml:"http://schema.foxinc.com/esni DistributionArea"`
-}
-
-type ChannelMetadata struct {
-	XMLName          xml.Name       `xml:"http://schema.foxinc.com/esni ChannelMetadata"`
-	AudienceType     string         `xml:"http://schema.foxinc.com/esni AudienceType,omitempty"`
-	LocationType     string         `xml:"http://schema.foxinc.com/esni LocationType,omitempty"`
-	Network          *FoxAttributes `xml:"http://schema.foxinc.com/esni Network,omitempty"`
-	Region           *FoxAttributes `xml:"http://schema.foxinc.com/esni Region,omitempty"`
-	DistributionArea *FoxAttributes `xml:"http://schema.foxinc.com/esni DistributionArea,omitempty"`
-}
-
-type DeviceMetadata struct {
-	XMLName      xml.Name `xml:"http://schema.foxinc.com/esni DeviceMetadata"`
-	AudienceType string   `xml:"http://schema.foxinc.com/esni AudienceType,omitempty"`
-}
-
-type LocationMetadata struct {
-	XMLName      xml.Name `xml:"http://schema.foxinc.com/esni LocationMetadata"`
-	AudienceType string   `xml:"http://schema.foxinc.com/esni AudienceType,omitempty"`
-}
-
-type FoxAttributes struct {
-	Name         string `xml:"name,attr,omitempty"`
-	Call_sign    string `xml:"call_sign,attr,omitempty"`
-	Network_type string `xml:"network_type,attr,omitempty"`
-}
-
-type DeliveryRestrictions struct {
-	XMLName            xml.Name            `xml:"http://schema.foxinc.com/esni DeliveryRestrictions,omitempty"`
-	WebEnabled         bool                `xml:"http://schema.foxinc.com/esni WebEnabled,omitempty"`
-	DeviceRestrictions *DeviceRestrictions `xml:"http://schema.foxinc.com/esni DeviceRestrictions,omitempty"`
-}
-
-type DeviceRestrictions struct {
-	XMLName xml.Name `xml:"http://schema.foxinc.com/esni DeviceRestrictions,omitempty"`
-	Match   string   `xml:"http://schema.foxinc.com/esni match,attr,omitempty"`
-	Devices []*Item  `xml:"http://schema.foxinc.com/esni Device,omitempty"`
-}
-
 //Table 10
 type Apply struct {
 	XMLName  xml.Name `xml:"http://www.scte.org/schemas/224 Apply"`
@@ -257,65 +156,21 @@ type Policy struct {
 	ReusableType
 	XMLName        xml.Name         `xml:"http://www.scte.org/schemas/224 Policy"`
 	ViewingPolicys []*ViewingPolicy `xml:"http://www.scte.org/schemas/224 ViewingPolicy,omitempty"`
-	Metadata       string           `xml:"http://www.scte.org/schemas/224 Metadata,omitempty"`
 }
 
 //Table 12
 type ViewingPolicy struct {
 	ReusableType
-	XMLName  xml.Name    `xml:"http://www.scte.org/schemas/224 ViewingPolicy"`
-	Audience *Audience   `xml:"http://www.scte.org/schemas/224 Audience,omitempty"`
-	Anys     []*Any      `xml:"http://www.scte.org/schemas/224 Any,omitempty"`
-	Contents []*AContent `xml:"urn:scte:224:action Content,omitempty"`
-	FF       []*Item     `xml:"urn:scte:224:action FastForward,omitempty"`
-	Capture  []*Capture  `xml:"urn:scte:224:action Capture,omitempty"`
-	Metadata string      `xml:"http://www.scte.org/schemas/224 Metadata,omitempty"`
+	XMLName  xml.Name  `xml:"http://www.scte.org/schemas/224 ViewingPolicy"`
+	Audience *Audience `xml:"http://www.scte.org/schemas/224 Audience,omitempty"`
 }
 
 //Table 13
 type Audience struct {
 	ReusableType
-	XMLName        xml.Name    `xml:"http://www.scte.org/schemas/224 Audience"`
-	Match          Match       `xml:"match,attr,omitempty"`
-	Metadata       *Metadata   `xml:"http://www.scte.org/schemas/224 Metadata,omitempty"`
-	Audiences      []*Audience `xml:"http://www.scte.org/schemas/224 Audience,omitempty"`
-	Virds          []*Item     `xml:"urn:scte:224:audience Vird,omitempty"`
-	Zips           []*Item     `xml:"urn:scte:224:audience Zip,omitempty"`
-	Features       []*Item     `xml:"urn:scte:224:audience Feature,omitempty"`
-	DeviceFeatures []*Item     `xml:"urn:scte:224:audience DeviceFeature,omitempty"`
-	Devices        []*Item     `xml:"urn:scte:224:audience Device,omitempty"`
-	FoxDevices     []*Item     `xml:"http://schema.foxinc.com/esni Device,omitempty"`
-	Networks       []*Item     `xml:"urn:scte:224:audience Network,omitempty"`
-	Default        []*Item     `xml:"urn:scte:224:audience Default,omitempty"`
-}
-
-type Capture struct {
-	XMLName     xml.Name     `xml:"urn:scte:224:action Capture"`
-	StartWindow *StartWindow `xml:"urn:scte:224:action StartWindow,omitempty"`
-	StopWindow  *StopWindow  `xml:"urn:scte:224:action StopWindow,omitempty"`
-	MidrollDAI  *Item        `xml:"urn:scte:224:action MidrollDAI,omitempty"`
-}
-
-type StartWindow struct {
-	Percentage *Item `xml:"urn:scte:224:action Percentage,omitempty"`
-}
-
-type StopWindow struct {
-	Offset     *Item `xml:"urn:scte:224:action Offset,omitempty"`
-	Percentage *Item `xml:"urn:scte:224:action Percentage,omitempty"`
-}
-
-type AContent struct {
-	XMLName xml.Name `xml:"urn:scte:224:action Content"`
-	Value   string   `xml:",chardata"`
-}
-type Item struct {
-	Value string `xml:",chardata"`
-}
-
-type Any struct {
-	XMLName xml.Name `xml:"http://www.scte.org/schemas/224 Any"`
-	Value   string   `xml:",chardata"`
+	XMLName   xml.Name    `xml:"http://www.scte.org/schemas/224 Audience"`
+	Match     Match       `xml:"match,attr,omitempty"`
+	Audiences []*Audience `xml:"http://www.scte.org/schemas/224 Audience,omitempty"`
 }
 
 //********************* Results Types *************************//
