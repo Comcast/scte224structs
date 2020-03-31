3fc23839bb35a3bc78bd9bf6e032197dd443c544
jcolwe200 <jonathan.colwell@comcast.com>
2020-03-31 15:58:33 -0700

    adding explicit support for ADI metadata to help support namespaces

---
+++ types/scte224v20180501/adi30/adi30.go
@@ -0,0 +1,175 @@
+package adi30
+
+// Package adi11 provides  structs for working with ADI 1.1 metadata files.
+// The CableLabs® Asset Distribution Interface Specification Version 1.1
+// (MD-SP-ADI1.1-C01-120803) provides a DTD to describe the file format.
+//
+// Within an ADI 1.1 file, App_Data and Content elements both have an attribute
+// named "Value". In these cases, the field is named ValueAttr to prevent duplication
+// since the encoding/xml package uses "Value" within the Attr struct to represent
+// the "value" in the attribute.
+
+import "encoding/xml"
+
+//ADI30 Top level element of an ADI 1.1 metadata file.
+type ADI30 struct {
+	XMLName xml.Name `xml:"http://www.scte.org/schemas/236/2017/core ADI3" json:"-"`
+	/*
+		Offer   string   `xml:"http://www.scte.org/schemas/236/2017/offer offer,attr"`
+		Terms   string   `xml:"http://www.scte.org/schemas/236/2017/termmmmms terms,attr" json:"-"`
+		Title   string   `xml:"http://www.scte.org/schemas/236/2017/title title,attr"`
+		Content string   `xml:"http://www.scte.org/schemas/236/2017/content content,attr" json:"-"`
+		Xsi     string   `xml:"http://www.w3.org/2001/XMLSchema-instance xsi,attr" json:"-"`
+		//Xmlns   string   `xml:"xmlns,attr" json:"-"`
+	*/
+	Asset []*Asset `xml:"http://www.scte.org/schemas/236/2017/core Asset,omitempty" json:"asset,omitempty"`
+	//Metadata *Metadata `xml:"Metadata"`
+}
+
+// Metadata elements are containers for a single AMS element and
+// zero or more App_Data elements.
+type Metadata struct {
+	XMLName xml.Name   `xml:"Metadata"`
+	Ams     *AMS       `xml:"AMS"`
+	AppData []*AppData `xml:"App_Data,omitempty"`
+}
+
+// An AMS element typically has a class of package, title, movie or poster (or box-cover).
+// The CableLabs specification indicates other @Asset_Class values including
+// preview, trickfile, encrypted and barker.
+type AMS struct {
+	XMLName      xml.Name `xml:"AMS"`
+	Product      string   `xml:"Product,attr"`
+	AssetID      string   `xml:"Asset_ID,attr"`
+	ProviderID   string   `xml:"Provider_ID,attr"`
+	Provider     string   `xml:"Provider,attr"`
+	AssetClass   string   `xml:"Asset_Class,attr"`
+	AssetName    string   `xml:"Asset_Name,attr"`
+	Description  string   `xml:"Description,attr"`
+	Verb         string   `xml:"Verb,attr,omitempty"`
+	VersionMinor string   `xml:"Version_Minor,attr"`
+	VersionMajor string   `xml:"Version_Major,attr"`
+	CreationDate string   `xml:"Creation_Date,attr"`
+	//Value        string   `xml:",chardata"`
+}
+
+// AppData (App_Data) specifies additional metadata not included in the AMS element.
+// Access to the contents of the "Value" attribute is provided via ValueAttr.
+type AppData struct {
+	XMLName   xml.Name `xml:"App_Data"`
+	App       string   `xml:"app,attr,omitempty"`
+	Name      string   `xml:"name,attr,omitempty"`
+	ValueAttr string   `xml:"value,attr,omitempty"`
+	//Value     string   `xml:",chardata"`
+}
+
+// The Asset element is a container for Metadata and Content elements and may
+// also contain child Asset elements
+type Asset struct {
+	//Type                 string `xml:"type,attr.omitempty"`
+	Type                 string `xml:"http://www.w3.org/2001/XMLSchema-instance type,attr"`
+	UriId                string `xml:"uriId,attr"`
+	ProviderVersionNum   string `xml:"providerVersionNum,attr"`
+	InternalVersionNum   string `xml:"internalVersionNum,attr"`
+	CreationDateTime     string `xml:"creationDateTime,attr"`
+	StartDateTime        string `xml:"startDateTime,attr"`
+	EndDateTime          string `xml:"endDateTime,attr"`
+	LastModifiedDateTime string `xml:"lastModifiedDateTime,attr"`
+
+	AlternateId                *altId          `xml:"http://www.scte.org/schemas/236/2017/core AlternateId,omitempty"`
+	ProviderQAContact          string          `xml:"http://www.scte.org/schemas/236/2017/core ProviderQAContact,omitempty"`
+	AssetName                  *deprecAndValue `xml:"http://www.scte.org/schemas/236/2017/core AssetName,omitempty"`
+	Provider                   string          `xml:"http://www.scte.org/schemas/236/2017/core Provider,omitempty"`
+	Description                *deprecAndValue `xml:"http://www.scte.org/schemas/236/2017/core Description,omitempty"`
+	OfrPres                    *presentation   `xml:"http://www.scte.org/schemas/236/2017/offer Presentation,omitempty"`
+	PromotionalContentGroupRef *UriId          `xml:"http://www.scte.org/schemas/236/2017/offer PromotionalContentGroupRef,omitempty"`
+	ProviderContentTier        string          `xml:"http://www.scte.org/schemas/236/2017/offer ProviderContentTier,omitempty"`
+	SourceMetadataSpecVersion  *deprecAndValue `xml:"http://www.scte.org/schemas/236/2017/offer SourceMetadataSpecVersion,omitempty"`
+	BillingId                  string          `xml:"http://www.scte.org/schemas/236/2017/offer BillingId,omitempty"`
+	TermsRef                   *UriId          `xml:"http://www.scte.org/schemas/236/2017/offer TermsRef,omitempty"`
+	ContentGroupRef            *UriId          `xml:"http://www.scte.org/schemas/236/2017/offer ContentGroupRef,omitempty"`
+
+	Ext              *Ext      `xml:"http://www.scte.org/schemas/236/2017/core Ext,omitempty"`
+	LocalizableTitle *LocTitle `xml:"http://www.scte.org/schemas/236/2017/title LocalizableTitle,omitempty"`
+
+	// LocalizableTitle *title `xml:"LocalizableTitle,omitempty"`
+
+	Rating             *Rating `xml:"http://www.scte.org/schemas/236/2017/title Rating,omitempty"`
+	IsClosedCaptioning string  `xml:"http://www.scte.org/schemas/236/2017/title IsClosedCaptioning,omitempty"`
+	DisplayRunTime     string  `xml:"http://www.scte.org/schemas/236/2017/title DisplayRunTime,omitempty"`
+	Year               int     `xml:"http://www.scte.org/schemas/236/2017/title Year,omitempty"`
+	Genre              string  `xml:"http://www.scte.org/schemas/236/2017/title Genre,omitempty"`
+	ShowType           string  `xml:"http://www.scte.org/schemas/236/2017/title ShowType,omitempty"`
+
+	AudioType           string              `xml:"http://www.scte.org/schemas/236/2017/content AudioType,omitempty"`
+	Language            *Language           `xml:"http://www.scte.org/schemas/236/2017/content Language,omitempty"`
+	TrickModeRestricted *Trickmodeexclusion `xml:"http://www.scte.org/schemas/236/2017/content TrickModesRestricted,omitempty"`
+	TitleRef            *UriId              `xml:"http://www.scte.org/schemas/236/2017/offer TitleRef,omitempty"`
+	MovieRef            *UriId              `xml:"http://www.scte.org/schemas/236/2017/offer MovieRef,omitempty"`
+	BillingGracePeriod  string              `xml:"http://www.scte.org/schemas/236/2017/terms BillingGracePeriod,omitempty"`
+	SuggestedPrice      string              `xml:"http://www.scte.org/schemas/236/2017/terms SuggestedPrice,omitempty"`
+	CategoryPath        string              `xml:"http://www.scte.org/schemas/236/2017/offer CategoryPath,omitempty"`
+}
+
+type Ext struct {
+	App_Data []*ExtAppData `xml:"App_Data,omitempty"`
+}
+
+type presentation struct {
+	CategoryRef *[]UriId `xml:"http://www.scte.org/schemas/236/2017/offer CategoryRef,omitempty"`
+}
+
+type altId struct {
+	IdentifierSystem string `xml:"identifierSystem,attr"`
+	Value            string `xml:",chardata"`
+}
+
+type deprecAndValue struct {
+	Deprecated bool   `xml:"deprecated,attr"`
+	Value      string `xml:",chardata"`
+}
+
+type UriId struct {
+	UriId string `xml:"uriId,attr,omitempty"`
+	// Value string `xml:"Value,chardata,omitempty"`
+}
+
+type ExtAppData struct {
+	Name  string `xml:"name,attr"`
+	Value string `xml:"value,attr"`
+}
+
+type Rating struct {
+	RatingSystem string `xml:"ratingSystem,attr"`
+	Value        string `xml:",chardata"`
+}
+
+type Language struct {
+	BitStreamMode string `xml:"bitStreamMode,attr"`
+	Value         string `xml:",chardata"`
+}
+
+type Trickmodeexclusion struct {
+	TrickModeExclusion *trickMode `xml:"TrickModeExclusion,omitempty"`
+}
+
+type trickMode struct {
+	Type string `xml:"type,attr,omitempty"`
+}
+
+// Content elements typically specify a file.
+// For example <Content  Value="/pid-fxnetworks.com-aid-DDDE0000103928336978.jpg"/>
+// Access to the "Value" attribute is provided via ValueAttr
+type Content struct {
+	XMLName   xml.Name `xml:"Content"`
+	ValueAttr string   `xml:"Value,attr"`
+	//Value     string   `xml:",chardata"`
+}
+
+type LocTitle struct {
+	TitleBrief   string `xml:"http://www.scte.org/schemas/236/2017/title TitleBrief,omitempty"`
+	TitleMedium  string `xml:"http://www.scte.org/schemas/236/2017/title TitleMedium,omitempty"`
+	TitleLong    string `xml:"http://www.scte.org/schemas/236/2017/title TitleLong,omitempty"`
+	SummaryShort string `xml:"http://www.scte.org/schemas/236/2017/title SummaryShort,omitempty"`
+	ActorDisplay string `xml:"http://www.scte.org/schemas/236/2017/title ActorDisplay,omitempty"`
+}
--- types/scte224v20180501/roundtrip_test.go
+++ types/scte224v20180501/roundtrip_test.go
@@ -135,3 +135,113 @@ func TestMetadataNamespace(t *testing.T) {
 		t.Fail()
 	}
 }
+
+const originalADIMetadata = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Media source="HBOHD" id="hbo.com/media/HBOHD" description="HBO HD" lastUpdated="2020-03-31T18:00:20.5Z" xmlns="http://www.scte.org/schemas/224" xmlns:signaling="http://www.scte.org/schemas/236/2017/signaling" xmlns:ad="http://www.scte.org/schemas/236/2017/ad" xmlns:lrm="https://lrm.aor.theplatform.com/lrm/schemas/esni/224" xmlns:title="http://www.scte.org/schemas/236/2017/title" xmlns:content="http://www.scte.org/schemas/236/2017/content" xmlns:offer="http://www.scte.org/schemas/236/2017/offer" xmlns:core="http://www.scte.org/schemas/236/2017/core" xmlns:terms="http://www.scte.org/schemas/236/2017/terms" xmlns:ns9="http://www.scte.org/schemas/35/2017" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <MediaPoint matchTime="2020-03-31T01:00:00Z" source="HBOHD" order="0" effective="2020-03-31T00:55:00Z" expires="2020-03-31T01:05:00Z" id="hbo.com/media/HBOHD/program/f2MhzKZcfiosBNjDM2hHGK0F8_Jv5fUR/start" description="The Plot Against America" lastUpdated="2020-03-31T18:00:20.5Z">
+    <AltID>ow7qaht5qULfWFcl5sbvzJXOVJEHsP6a</AltID>
+    <Metadata>
+      <core:ADI3>
+        <core:Asset xsi:type="content:MovieType" providerVersionNum="20" internalVersionNum="20" creationDateTime="2020-03-30T00:00:00" startDateTime="2020-03-30T18:00:00.000-07:00" endDateTime="2020-03-30T19:01:00.000-07:00" lastModifiedDateTime="2020-03-24T23:21:02.000-07:00" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+          <core:Provider>HBO</core:Provider>
+          <content:Language bitStreamMode="2">eng</content:Language>
+        </core:Asset>
+        <core:Asset xsi:type="title:TitleType" uriId="hbo.com/title/HBO" providerVersionNum="20" internalVersionNum="20" creationDateTime="2020-03-30T00:00:00" startDateTime="2020-03-30T18:00:00.000-07:00" endDateTime="2020-03-30T19:01:00.000-07:00" lastModifiedDateTime="2020-03-24T23:21:02.000-07:00" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+          <core:Provider>HBO</core:Provider>
+          <core:Ext>
+            <core:App_Data name="Season_Number" value="1"/>
+            <core:App_Data name="Season_Episode_Number" value="3"/>
+            <core:App_Data name="Series_Episode_Number" value="3"/>
+            <core:App_Data name="Category" value="Other"/>
+            <core:App_Data name="Genre" value="Drama"/>
+          </core:Ext>
+          <title:LocalizableTitle>
+            <title:TitleBrief>Plot Against America</title:TitleBrief>
+            <title:TitleMedium>The Plot Against America</title:TitleMedium>
+            <title:TitleLong>The Plot Against America</title:TitleLong>
+            <title:SummaryShort>Based on Philip Roth's novel of an alternate American history during World War II.</title:SummaryShort>
+          </title:LocalizableTitle>
+          <title:Rating ratingSystem="urn:v-chip">TVMA</title:Rating>
+          <title:IsClosedCaptioning>true</title:IsClosedCaptioning>
+          <title:DisplayRunTime>60</title:DisplayRunTime>
+          <title:Year>2020</title:Year>
+        </core:Asset>
+      </core:ADI3>
+      <lrm:MetadataDetail name="ScheduledStart" type="timestamp" provider="HBO">2020-03-31T01:00:00Z</lrm:MetadataDetail>
+      <lrm:MetadataDetail name="ScheduledEnd" type="timestamp" provider="HBO">2020-03-31T02:01:00Z</lrm:MetadataDetail>
+      <lrm:MetadataDetail name="StartOver" type="Boolean" provider="HBO">false</lrm:MetadataDetail>
+      <lrm:MetadataDetail name="StartOverDuration" type="String" provider="HBO"></lrm:MetadataDetail>
+      <lrm:MetadataDetail name="Lookback" type="Boolean" provider="HBO">false</lrm:MetadataDetail>
+    </Metadata>
+  </MediaPoint>
+</Media>`
+
+const expectedInlinedADIMetadata = `<Media xmlns="http://www.scte.org/schemas/224" id="hbo.com/media/HBOHD" description="HBO HD" lastUpdated="2020-03-31T18:00:20.5Z" source="HBOHD">
+  <MediaPoint xmlns="http://www.scte.org/schemas/224" id="hbo.com/media/HBOHD/program/f2MhzKZcfiosBNjDM2hHGK0F8_Jv5fUR/start" description="The Plot Against America" lastUpdated="2020-03-31T18:00:20.5Z" effective="2020-03-31T00:55:00Z" expires="2020-03-31T01:05:00Z" matchTime="2020-03-31T01:00:00Z" source="HBOHD" order="0">
+    <AltID xmlns="http://www.scte.org/schemas/224">ow7qaht5qULfWFcl5sbvzJXOVJEHsP6a</AltID>
+    <Metadata xmlns="http://www.scte.org/schemas/224">
+      <ADI3 xmlns="http://www.scte.org/schemas/236/2017/core">
+        <Asset xmlns="http://www.scte.org/schemas/236/2017/core" xmlns:XMLSchema-instance="http://www.w3.org/2001/XMLSchema-instance" XMLSchema-instance:type="content:MovieType" uriId="" providerVersionNum="20" internalVersionNum="20" creationDateTime="2020-03-30T00:00:00" startDateTime="2020-03-30T18:00:00.000-07:00" endDateTime="2020-03-30T19:01:00.000-07:00" lastModifiedDateTime="2020-03-24T23:21:02.000-07:00">
+          <Provider xmlns="http://www.scte.org/schemas/236/2017/core">HBO</Provider>
+          <Language xmlns="http://www.scte.org/schemas/236/2017/content" bitStreamMode="2">eng</Language>
+        </Asset>
+        <Asset xmlns="http://www.scte.org/schemas/236/2017/core" xmlns:XMLSchema-instance="http://www.w3.org/2001/XMLSchema-instance" XMLSchema-instance:type="title:TitleType" uriId="hbo.com/title/HBO" providerVersionNum="20" internalVersionNum="20" creationDateTime="2020-03-30T00:00:00" startDateTime="2020-03-30T18:00:00.000-07:00" endDateTime="2020-03-30T19:01:00.000-07:00" lastModifiedDateTime="2020-03-24T23:21:02.000-07:00">
+          <Provider xmlns="http://www.scte.org/schemas/236/2017/core">HBO</Provider>
+          <Ext xmlns="http://www.scte.org/schemas/236/2017/core">
+            <App_Data name="Season_Number" value="1"></App_Data>
+            <App_Data name="Season_Episode_Number" value="3"></App_Data>
+            <App_Data name="Series_Episode_Number" value="3"></App_Data>
+            <App_Data name="Category" value="Other"></App_Data>
+            <App_Data name="Genre" value="Drama"></App_Data>
+          </Ext>
+          <LocalizableTitle xmlns="http://www.scte.org/schemas/236/2017/title">
+            <TitleBrief xmlns="http://www.scte.org/schemas/236/2017/title">Plot Against America</TitleBrief>
+            <TitleMedium xmlns="http://www.scte.org/schemas/236/2017/title">The Plot Against America</TitleMedium>
+            <TitleLong xmlns="http://www.scte.org/schemas/236/2017/title">The Plot Against America</TitleLong>
+            <SummaryShort xmlns="http://www.scte.org/schemas/236/2017/title">Based on Philip Roth&#39;s novel of an alternate American history during World War II.</SummaryShort>
+          </LocalizableTitle>
+          <Rating xmlns="http://www.scte.org/schemas/236/2017/title" ratingSystem="urn:v-chip">TVMA</Rating>
+          <IsClosedCaptioning xmlns="http://www.scte.org/schemas/236/2017/title">true</IsClosedCaptioning>
+          <DisplayRunTime xmlns="http://www.scte.org/schemas/236/2017/title">60</DisplayRunTime>
+          <Year xmlns="http://www.scte.org/schemas/236/2017/title">2020</Year>
+        </Asset>
+      </ADI3>
+      <MetadataDetail xmlns="https://lrm.aor.theplatform.com/lrm/schemas/esni/224" name="ScheduledStart" type="timestamp" provider="HBO">2020-03-31T01:00:00Z</MetadataDetail>
+      <MetadataDetail xmlns="https://lrm.aor.theplatform.com/lrm/schemas/esni/224" name="ScheduledEnd" type="timestamp" provider="HBO">2020-03-31T02:01:00Z</MetadataDetail>
+      <MetadataDetail xmlns="https://lrm.aor.theplatform.com/lrm/schemas/esni/224" name="StartOver" type="Boolean" provider="HBO">false</MetadataDetail>
+      <MetadataDetail xmlns="https://lrm.aor.theplatform.com/lrm/schemas/esni/224" name="StartOverDuration" type="String" provider="HBO"></MetadataDetail>
+      <MetadataDetail xmlns="https://lrm.aor.theplatform.com/lrm/schemas/esni/224" name="Lookback" type="Boolean" provider="HBO">false</MetadataDetail>
+    </Metadata>
+  </MediaPoint>
+</Media>`
+
+func TestADIMetadata(t *testing.T) {
+	decoder := xml.NewDecoder(strings.NewReader(originalADIMetadata))
+	var metaScte Media
+	decodeErr := decoder.Decode(&metaScte)
+	if nil != decodeErr {
+		t.Log(decodeErr)
+		t.FailNow()
+	}
+	if metaScte.Source != "HBOHD" {
+		t.Log(metaScte.Source, "should have been \"HBOHD\"")
+		t.Fail()
+	}
+	if 1 != len(metaScte.MediaPoints) {
+		t.Log("Expected just one MediaPoint")
+		t.Fail()
+	}
+
+	pretty, marshalErr := xml.MarshalIndent(metaScte, "", "  ")
+	if nil != marshalErr {
+		t.Log(marshalErr)
+		t.FailNow()
+	}
+	roundtrip := string(pretty)
+	if expectedInlinedADIMetadata != roundtrip {
+		t.Log(roundtrip)
+		t.Log("did not match")
+		t.Log(expectedInlinedADIMetadata)
+		t.Fail()
+	}
+}
